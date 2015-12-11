737860b2f3f17dbbc4dc97ed51192cebe587ca70
mihail1986 <yahim09@gmail.com>
2015-12-11 03:10:29 +0200

    I added admin controll of the modle Gallery

---
+++ admin/language/english/gallery.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']    = 'Gallery';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified gallery module!';
+$_['text_edit']        = 'Edit Gallery Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_gallery']     = 'Gallery';
+$_['entry_dimension']  = 'Dimension (W x H) and Resize Type';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify gallery module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
